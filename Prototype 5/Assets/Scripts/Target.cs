debe36e0d209a28b6c896d8ebbd3e88d99eadfc8
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2020-03-08 19:03:10 -0700

    5.2.7 c

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public int pointValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +31,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.updateScore(5);
+        gameManager.updateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
