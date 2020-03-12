eba648af711288fca32ee58ffd8a300971f9fe28
Dhruv Pareek <dp00888@srcs.loc>
2020-03-12 14:23:21 -0700

    5.4.4 c

--- Prototype 5/Assets/Scripts/DifficultyButton.cs
+++ Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -11,11 +11,18 @@ public class DifficultyButton : MonoBehaviour
     void Start()
     {
         button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
     }
 }
